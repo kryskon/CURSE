5665103a93357952eaebb877729ea3e3f9cd780c
kryskon <51208504+kryskon@users.noreply.github.com>
2019-07-02 13:47:24 -0400

    Updated code
    Fixed the login function

--- classes.h
+++ classes.h
@@ -1,29 +1,8 @@
 #include <string>
 #include <iostream>
-#include <vector>
 #include <list>
 
 using namespace std;
-
-class course{
-  protected:
-    int CRN;
-    string minYear, subject, instructorUsername; //must be junior, senior, etc to register for this class
-    list<student> students;
-
-  public:
-    course(){}
-    course(int c, string y, string s, string i){CRN = c, minYear = y, subject = s, instructorUsername = i;}
-    ~course(){}
-
-    void setCRN(int c){CRN = c;}
-    int getCRN(){return CRN;}
-    void setMinYear(string y){minYear = y;}
-    string getMinYear(){return minYear;}
-    void setSubject(string s){subject = s;}
-    string getSubject(){return subject;}
-
-};
 
 class user{
   protected:
@@ -44,14 +23,14 @@ class user{
     void setPassword(string p){password = p;}
     string getPassword(){return password;}
 
-    virtual void options(){
+    virtual int options(){
       //this is where users can choose an action based on user type
       //virtual so each class has it's own appropriate options
     }
 };
 
 list<user> userList; //holds all users until database integration
-list<course> courseList; //holds all available courses until database integration
+
 
 class student:public user{
   protected:
@@ -74,14 +53,7 @@ class student:public user{
       cin >> crn;
       //search database for crn. Find course and requirements
       //compare prereqs and class sizez. If no issues:
-      classes.push_back(crn);
-    }
-
-    void viewSchedule(){
-        for(list<int>::iterator ptr = classes.begin(); ptr != classes.end(); ptr++){
-          //take crn and lookup in database
-          //return class info (students, time, etc)
-        }
+      classes.push_front(crn);
     }
 
     void dropClass(){
@@ -95,10 +67,11 @@ class student:public user{
       }
     }
 
-    void options(){
+    int options(){
       //options here
     }
 };
+list<student> studentList;
 
 class instructor:public user{
   protected:
@@ -107,11 +80,11 @@ class instructor:public user{
 
   public:
     instructor(){}
-    instructor(string s){instructorSubject = s;}
+    instructor(string fn, string ln, string u, string p, string s){firstName = fn, lastName = ln, userName = u, password = p, instructorSubject = s;}
     ~instructor(){}
 
-    void setInstructorType(string s){instructorSubject = s;}
-    string getInstructorType(){return instructorType;}
+    void setInstructorSubject(string s){instructorSubject = s;}
+    string getInstructorSubject(){return instructorSubject;}
 
     void viewSchedule(){
         for(vector<int>::iterator ptr = classes.begin(); ptr != classes.end(); ptr++){
@@ -124,6 +97,30 @@ class instructor:public user{
       //options here
     }
 };
+
+list<instructor> instructorList;
+
+class course{
+  protected:
+    int CRN;
+    string minYear, subject, instructorUsername; //must be junior, senior, etc to register for this class
+    list<student> students;
+
+  public:
+    course(){}
+    course(int c, string y, string s, string i){CRN = c, minYear = y, subject = s, instructorUsername = i;}
+    ~course(){}
+
+    void setCRN(int c){CRN = c;}
+    int getCRN(){return CRN;}
+    void setMinYear(string y){minYear = y;}
+    string getMinYear(){return minYear;}
+    void setSubject(string s){subject = s;}
+    string getSubject(){return subject;}
+
+};
+list<course> courseList; //holds all available courses until database integration
+
 
 class admin:public user{
   protected:
@@ -140,38 +137,7 @@ class admin:public user{
         cin >> name;
         //scan for user. view thier schedule
     }
-    /*
-    void manageClasses(){
-      //have access to whole database
-      int choice;
-      cout << "Enter 1 to add a class\nEnter 2 to delete a class\nEnter 3 to edit a class\n";
-      cin >> choice;
-      switch(choice){
-        case 1:
-          int addCRN;
-          cout << "Enter the crn of the class you want to add: ";
-          cin >> addCRN;
-          //add an entry in the database for this course
-          //fill in course details (instrutor, capacity, time, etc)
-          break;
-        case 2:
-          int delCRN;
-          cout << "Enter the crn of the lass you want to delete: ";
-          cin >> delCRN;
-          //scan database for matching crn
-          //remove course from database if found
-          break;
-        case 3:
-          int editCRN;
-          cout << "Enter the crn of the course you wish to edit: ";
-          cin >> editCRN;
-          //search database and find matching crn
-          //specify which attribute to modify
-          //make changes
-          break;
-      }
-    }
-    */
+
     void addCourse(){
       int c;
       string y, s, i;
@@ -187,7 +153,7 @@ class admin:public user{
       courseList.push_front(newCourse);
 
     }
-    void removeCourse(){
+    int removeCourse(){
       int c;
       cout << "Enter the CRN of the course you wish to remove: ";
       cin >> c;
@@ -195,13 +161,16 @@ class admin:public user{
         if(ptr->getCRN() == c){
           courseList.erase(ptr);
           cout << "\nCourse successfully removed.\n";
+          return 0;
         }
       }
+      cout << "Error, course doesn't exist.\n";
+      return 0;
     }
 
 
     void addStudent(){
-      string fn, ln, u, p, m y;
+      string fn, ln, u, p, m, y;
 
       cout << "Enter a first name: ";
       cin >> fn;
@@ -215,12 +184,26 @@ class admin:public user{
       userList.push_front(newStudent); //always push to front
     }
 
-    void options(){
+    int options(){
+      int userChoice;
+      cout << "Enter 1 to add a course\nEnter 2 to remove a course\nEnter 3 to logout\n";
+      cin >> userChoice;
+      switch(userChoice){
+        case 1:
+          this->addCourse();
+          break;
+        case 2:
+          this->removeCourse();
+          break;
+        case 3:
+          return 0;
+          break;  //need to somehow logout here
+      }
       //options here
     }
 };
 
-
+list<admin> adminList;
 
 list<user>::iterator attemptlogin(string n, string p){ //return a pointer to some object in the list
   list<user>::iterator ptr;
@@ -228,6 +211,36 @@ list<user>::iterator attemptlogin(string n, string p){ //return a pointer to som
     if((ptr->getUserName() == n) && (ptr->getPassword() == p)){return ptr;} //if match, return pointer
   }
   for(ptr = userList.begin(); ptr != userList.end(); ptr++){  //if no match, return NULL pointer
+    if(ptr->getUserName() == "NULL"){return ptr;}
+  }
+}
+
+list<student>::iterator attemptloginS(string n, string p){
+  list<student>::iterator ptr;
+  for(ptr = studentList.begin(); ptr != studentList.end(); ptr++){ //search list for matching username
+    if((ptr->getUserName() == n) && (ptr->getPassword() == p)){return ptr;} //if match, return pointer
+  }
+  for(ptr = studentList.begin(); ptr != studentList.end(); ptr++){  //if no match, return NULL pointer
+    if(ptr->getUserName() == "NULL"){return ptr;}
+  }
+}
+
+list<instructor>::iterator attemptloginI(string n, string p){
+  list<instructor>::iterator ptr;
+  for(ptr = instructorList.begin(); ptr != instructorList.end(); ptr++){ //search list for matching username
+    if((ptr->getUserName() == n) && (ptr->getPassword() == p)){return ptr;} //if match, return pointer
+  }
+  for(ptr = instructorList.begin(); ptr != instructorList.end(); ptr++){  //if no match, return NULL pointer
+    if(ptr->getUserName() == "NULL"){return ptr;}
+  }
+}
+
+list<admin>::iterator attemptloginA(string n, string p){
+  list<admin>::iterator ptr;
+  for(ptr = adminList.begin(); ptr != adminList.end(); ptr++){ //search list for matching username
+    if((ptr->getUserName() == n) && (ptr->getPassword() == p)){return ptr;} //if match, return pointer
+  }
+  for(ptr = adminList.begin(); ptr != adminList.end(); ptr++){  //if no match, return NULL pointer
     if(ptr->getUserName() == "NULL"){return ptr;}
   }
 }
--- main.cpp
+++ main.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
 #include <string>
 #include <list>
-#include <vector>
 #include "classes.h"
 
 using namespace std;
 
-
 int main(){
-	user nullUser("NULL", "NULL", "NULL", "NULL"); //NULL object. Very important, don't delete
+	user nullUser("NULL", "NULL", "NULL", "NULL"); //NULL objects. Very important, don't delete
 	userList.push_back(nullUser);
+	student nullStudent("NULL", "NULL", "NULL", "NULL", "NULL", "NULL");
+	studentList.push_back(nullStudent);
+	instructor nullInstructor("NULL", "NULL", "NULL", "NULL", "NULL");
+	instructorList.push_back(nullInstructor);
+	admin nullAdmin("NULL", "NULL", "NULL", "NULL", "NULL");
+	adminList.push_back(nullAdmin);
 	admin newAdmin("Nick", "Krysko", "password1", "kryskon", "sysadmin"); //need at least one default admin to start
-	userList.push_front(newAdmin); //make sure all new users get pushed to the front, NOT THE BACK
+	adminList.push_front(newAdmin); //make sure all new users get pushed to the front, NOT THE BACK
 	string usrN;
 	string usrP;
+	list<student>::iterator user1; //potential user type student
+	list<instructor>::iterator user2; //potential user type instructor
+	list<admin>::iterator user3; //potential user type admin
+	int type;
 	int logout = 0;
 	cout << "Enter your username: ";
 	cin >> usrN;
 	cout << "Enter your password: ";
 	cin >> usrP;
-	list<user>::iterator user = attemptlogin(usrN, usrP); //find the user, return it's pointer
-	if(user->getUserName()!="NULL"){ //if successfull login:
-		while(!logout){
-			user->options();
-			//start main loop here. This is where the switch statement will go with different options, aka the options() virtual funtion
-		}
-		//user has logged out. Back to login menu?
+	user1 = attemptloginS(usrN, usrP);
+	if (user1->getUserName()=="NULL"){
+		user2 = attemptloginI(usrN, usrP);
+			if(user2->getUserName()=="NULL"){
+				user3 = attemptloginA(usrN, usrP);
+				type = 3;
+			}
+			else{type = 2;}
 	}
-	else{ //unsuccessfull login. should put in loop so it gives another attempt instead of stopping here
-		cout << "Error, invalid username or password\n";
+	else{type = 1;}
+	if(type == 1){ //if user is a student...
+		while(!logout){
+			user1->options();
+		}
+	}
+	else if(type == 2){ //if user is an instructor...
+		while(!logout){
+			user2->options();
+		}
+	}
+	else {
+		if (user3->getUserName() != "NULL"){ //if user is an admin
+			while(!logout){
+				user3->options();
+			}
+		}
+		else {
+			cout << "Error, invalid username or password\n";
+		}
 	}
 }
