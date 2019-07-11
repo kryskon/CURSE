7e9789514417bbfecb5cc566cb89fc6da3502944
kryskon <51208504+kryskon@users.noreply.github.com>
2019-07-11 17:37:41 -0400

    Improved logout function
    Made it so that logging out bring you back to the login screen instead of just exiting the program

--- main.cpp
+++ main.cpp
@@ -46,39 +46,48 @@ int main(){
 	list<instructor>::iterator user2; //potential user type instructor
 	list<admin>::iterator user3; //potential user type admin
 	int type;
-	int logout = 0;
-	cout << "Enter your username: ";
-	cin >> usrN;
-	cout << "Enter your password: ";
-	cin >> usrP;
-	user1 = attemptloginS(usrN, usrP); //cehck if user is a student
-	if (user1->getUserName()=="NULL"){
-		user2 = attemptloginI(usrN, usrP); //check if user is an instructor
-			if(user2->getUserName()=="NULL"){
-				user3 = attemptloginA(usrN, usrP); //check if user is an admin
-				type = 3;
+	int exit = 0;
+	while(!exit){
+		int logout = 0;
+		int userChoice;
+		cout << "Enter 1 to login\nEnter 2 to exit the program\n";
+		cin >> userChoice;
+		if(userChoice == 1){ //if logging in...
+			cout << "Enter your username: ";
+			cin >> usrN;
+			cout << "Enter your password: ";
+			cin >> usrP;
+			user1 = attemptloginS(usrN, usrP); //cehck if user is a student
+			if (user1->getUserName()=="NULL"){
+				user2 = attemptloginI(usrN, usrP); //check if user is an instructor
+					if(user2->getUserName()=="NULL"){
+						user3 = attemptloginA(usrN, usrP); //check if user is an admin
+						type = 3;
+					}
+					else{type = 2;}
 			}
-			else{type = 2;}
-	}
-	else{type = 1;}
-	if(type == 1){ //if user is a student...
-		while(!logout){
-			logout = user1->options();
-		}
-	}
-	else if(type == 2){ //if user is an instructor...
-		while(!logout){
-			logout = user2->options();
-		}
-	}
-	else {
-		if (user3->getUserName() != "NULL"){ //if user is an admin...
-			while(!logout){
-				logout = user3->options();
+			else{type = 1;}
+			if(type == 1){ //if user is a student...
+				while(!logout){
+					logout = user1->options();
+				}
+			}
+			else if(type == 2){ //if user is an instructor...
+				while(!logout){
+					logout = user2->options();
+				}
+			}
+			else {
+				if (user3->getUserName() != "NULL"){ //if user is an admin...
+					while(!logout){
+						logout = user3->options();
+					}
+				}
+				else {
+					cout << "Error, invalid username or password\n";
+				}
 			}
 		}
-		else {
-			cout << "Error, invalid username or password\n";
-		}
+		else exit = 1; //exit program
 	}
 }
