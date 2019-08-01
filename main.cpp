451ad005de456f0fe15b7c70c816a9e794e60f6c
kryskon <51208504+kryskon@users.noreply.github.com>
2019-08-01 12:48:07 -0400

    adding student option stuff

--- main.cpp
+++ main.cpp
@@ -32,11 +32,12 @@ int main(int argc, char** argv){
 	int type;
 	int done = 0;
 	int userChoice = 0;
-
+	int step;
 	//begin main loop
 	while(!done){
 		int logout = 0;
 		int userChoice;
+		cout << "\n---------------------------------------------\n";
 		cout << "Enter 1 to login\nEnter 2 to exit the program\n";
 		cin >> userChoice;
 		if(userChoice == 1){ //if logging in...
@@ -46,7 +47,7 @@ int main(int argc, char** argv){
 			cin >> usrP;
 
 			//begin login process
-			query = "SELECT EMAIL, PASSWORD FROM ADMIN WHERE EMAIL = '" + usrN + "';"; //query to find result
+			query = "SELECT EMAIL, PASSWORD FROM ADMIN WHERE EMAIL = '" + usrN + "';"; //query to find result (email is the same as username)
 			const char *c = query.c_str();
 			sqlite3_prepare_v2(DB, c, -1, &res, 0);
 			sqlite3_bind_int(res, 1, 3);
@@ -82,19 +83,39 @@ int main(int argc, char** argv){
 							c = query.c_str();
 							sqlite3_prepare_v2(DB, c, -1, &res, 0);
 							sqlite3_bind_int(res, 1, 3);
-							int step = sqlite3_step(res);
+							step = sqlite3_step(res);
 							if (step == SQLITE_ROW){		//if query actually has a row (i.e match found)
 								string QresultU = string(reinterpret_cast<const char*>(sqlite3_column_text(res, 0))); //have to convert const char* to string
 								string QresultP = string(reinterpret_cast<const char*>(sqlite3_column_text(res, 1)));
 								if (QresultU == usrN && QresultP == usrP){ //successful student login
 									cout << "Login Successful\n";
+									student currentStudent(); //create new object with all info here to do comparisons 
 									while(!logout){
 										cout << "\nEnter 1 to view all available courses\nEnter 2 to register for courses\nEnter 3 to drop a class\nEnter 4 to list enrolled classes\nEnter 5 to logout\n";
 									  cin >> userChoice;
 										switch(userChoice){
 											case 1: //view all courses
+												cout << "\n----------------All Courses----------------\n";
 												query = "SELECT * FROM COURSE;";
 												sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
+												cout << "---------------------------------------------\n";
+												break;
+											case 2: //going to need to give each class it's own table to hold student rosters
+												int crn;
+												cout << "\nPlease enter the CRN for your desired course: ";
+												cin >> crn;
+												query = "SELECT CRN FROM COURSE WHERE CRN = " + crn + " ;";
+												c = query.c_str();
+												sqlite3_prepare_v2(DB, c, -1, &res, 0);
+												sqlite3_bind_int(res, 1, 3);
+												step = sqlite3_step(res);
+												if (step == SQLITE_ROW){
+													QresultU = string(reinterpret_cast<const char*>(sqlite3_column_text(res, 0)));
+													if (QresultU == crn){
+														sql = "INSERT INTO COURSE_" + crn + " (ID, NAME, SURNAME, EMAIL) SELECT ID, NAME, SURNMAE, EMAIL FROM STUDENT WHERE ;"; //fill student info into values
+													}
+												}
+
 												break;
 											case 5:
 												logout = 1;
